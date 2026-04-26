d6d0efea8453e732c4b05a3c4d0085c05291726a
EdmirSoares <dimisoares10@gmail.com>
2026-04-26 15:03:40 -0300

    chore: update dependencies to include @tanstack/react-query and axios

---
+++ app.config.js
@@ -0,0 +1,36 @@
+/** @type {import('expo/config').ExpoConfig} */
+module.exports = {
+  name: 'clody',
+  slug: 'clody',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
+  newArchEnabled: true,
+  splash: {
+    image: './assets/splash-icon.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'com.edmirsoares.clody',
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#ffffff',
+    },
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+    package: 'com.edmirsoares.clody',
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  plugins: ['expo-router'],
+  scheme: 'clody',
+  extra: {
+    owmApiKey: process.env.OWM_API_KEY,
+  },
+}
